0613fcd8d17c2c0fb630c7cbe19518e0c9effff5
Matheus Lippe <72580051+matheuslippe@users.noreply.github.com>
2024-11-16 22:34:20 -0300

    feat: ensure transaction can be updated

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -7,10 +7,10 @@ import {
   TransactionPaymentMethod,
   TransactionType,
 } from "@prisma/client";
-import { addTransactionSchema } from "./schema";
+import { upsertTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
-interface AddTransactionSchema {
+interface UpsertTransactionSchema {
   name: string;
   amount: number;
   type: TransactionType;
@@ -19,14 +19,18 @@ interface AddTransactionSchema {
   date: Date;
 }
 
-export const addTransaction = async (params: AddTransactionSchema) => {
-  addTransactionSchema.parse(params);
+export const upsertTransaction = async (params: UpsertTransactionSchema) => {
+  upsertTransactionSchema.parse(params);
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
-  await db.transaction.create({
-    data: { ...params, userId },
+  await db.transaction.upsert({
+    where: {
+      id: params.id,
+    },
+    update: { ...params, userId },
+    create: { ...params, userId },
   });
   revalidatePath("/transaction");
 };
